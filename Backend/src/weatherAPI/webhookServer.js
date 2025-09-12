6074fd9d3164cf8aa99b6582f813b0ecd9ded028
Alejo D'Aloia <alejodaloia03@gmail.com>
2025-09-12 00:52:45 -0300

    backend finalizado, se consulta y se envia informacion correctamente a la base de datos

---
+++ Backend/src/weatherAPI/webhookServer.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+app.post("/webhook", async (req, res) => {
+  const weather = {
+    ...req.body,
+    timestamp_utc: new Date().toISOString(), // Timestamp UTC
+  };
+
+  console.log("📥 Webhook recibió:", weather);
+
+  try {
+    const { error } = await supabase.from("weather_data").insert([weather]);
+
+    if (error) throw error;
+
+    res.json({ status: "ok" });
+  } catch (err) {
+    console.error("❌ Error guardando en Supabase:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(4000, () =>
+  console.log("📩 Webhook server escuchando en http://localhost:4000/webhook")
+);
